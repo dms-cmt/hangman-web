21de6f46a56159a49f1efa88cab034e576d126cc
Milos Zivlak <zivlakmilos@gmail.com>
2016-05-13 19:55:23 +0200

    Ubacena konekcija prilikom otvaranja sesije, treba testirati

--- Global.asax.cs
+++ Global.asax.cs
@@ -3,18 +3,21 @@ using System.Collections;
 using System.ComponentModel;
 using System.Web;
 using System.Web.SessionState;
+using System.ServiceModel;
+
+using HangmanService;
 
 namespace hangmanweb
 {
 	public class Global : System.Web.HttpApplication
 	{
-		
 		protected virtual void Application_Start (Object sender, EventArgs e)
 		{
 		}
 		
 		protected virtual void Session_Start (Object sender, EventArgs e)
 		{
+			Connect ();
 		}
 		
 		protected virtual void Application_BeginRequest (Object sender, EventArgs e)
@@ -39,6 +42,17 @@ namespace hangmanweb
 		
 		protected virtual void Application_End (Object sender, EventArgs e)
 		{
+		}
+
+		/*
+		 * Otvaranje konekcije na servis
+		 */
+		private void Connect ()
+		{
+			var security = new SecurityMode ();
+			var binding = new WSHttpBinding (security, true);
+			var address = new EndpointAddress ("http://localhost:8325/");
+			Session ["client"] = new HangmanClient (binding, address);
 		}
 	}
 }
